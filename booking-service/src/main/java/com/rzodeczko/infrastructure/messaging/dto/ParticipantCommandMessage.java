0818720691e941455f12931e54315215f3f1c407
CoderNoOne <firelight.code@gmail.com>
2026-07-11 15:57:53 +0200

    refactor message records to improve readability by formatting parameters

--- booking-service/src/main/java/com/rzodeczko/infrastructure/messaging/dto/ParticipantCommandMessage.java
+++ booking-service/src/main/java/com/rzodeczko/infrastructure/messaging/dto/ParticipantCommandMessage.java
@@ -4,5 +4,9 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public record ParticipantCommandMessage(
-        UUID sagaId, String action, String customerName, String destination, BigDecimal amount
+        UUID sagaId,
+        String action,
+        String customerName,
+        String destination,
+        BigDecimal amount
 ) { }
--- booking-service/src/main/java/com/rzodeczko/infrastructure/messaging/dto/SagaReplyMessage.java
+++ booking-service/src/main/java/com/rzodeczko/infrastructure/messaging/dto/SagaReplyMessage.java
@@ -2,4 +2,10 @@ package com.rzodeczko.infrastructure.messaging.dto;
 
 import java.util.UUID;
 
-public record SagaReplyMessage(UUID sagaId, String step, String action, String status, String reason) { }
+public record SagaReplyMessage(
+        UUID sagaId,
+        String step,
+        String action,
+        String status,
+        String reason) {
+}
