71d3548c101c0e2e228286f68035330fcf13a982
Michał Rzodeczko <firelight.code@gmail.com>
2026-07-30 13:25:29 +0200

    remove unecssary database cleanup method from IntegrationTestBase

--- booking-service/src/test/java/com/rzodeczko/IntegrationTestBase.java
+++ booking-service/src/test/java/com/rzodeczko/IntegrationTestBase.java
@@ -1,18 +1,11 @@
 package com.rzodeczko;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Import({RabbitMqTestcontainersConfig.class, MySqlTestcontainerConfig.class})
 @Testcontainers(disabledWithoutDocker = true)
 public abstract class IntegrationTestBase {
-
-    @BeforeEach
-    @Sql(statements = {"delete from outbox_event", "delete from saga_step", "delete fromłó saga_instance"})
-    void cleanDatabase() {
-    }
 }
