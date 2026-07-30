874b9e22903fea71b30399b41d0d35bbc92f8853
Michał Rzodeczko <firelight.code@gmail.com>
2026-07-30 12:43:22 +0200

    feat(test): add database cleanup before each integration test

--- booking-service/src/test/java/com/rzodeczko/IntegrationTestBase.java
+++ booking-service/src/test/java/com/rzodeczko/IntegrationTestBase.java
@@ -1,9 +1,16 @@
 package com.rzodeczko;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.jdbc.Sql;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Import(TestcontainersConfiguration.class)
 public abstract class IntegrationTestBase {
+
+    @BeforeEach
+    @Sql(statements = {"truncate table outbox_event", "TRUNCATE TABLE saga_step", "TRUNCATE TABLE saga_instance"})
+    void cleanDatabase() {
+    }
 }
